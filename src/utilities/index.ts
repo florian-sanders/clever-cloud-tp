c1df1b0b0bd8bc4d263c11655c9bc496fdeb4b39
Florian Sanders <sanders.florian@gmail.com>
2021-11-21 23:54:41 +0100

    feat: added puralize helper

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -2,3 +2,5 @@ export const getFormattedRam = (mem: number): string =>
 	mem < 1000 ? `${mem} MiB` : `${Math.floor(mem / 1000).toFixed(0)} GiB`;
 
 export const getFormattedPrice = (price: number): string => (price * 41.904).toFixed(2);
+
+export const pluralize = (dependency: number, singularWord: string): string => dependency > 1 ? `${singularWord}s` : singularWord; 
