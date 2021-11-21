60e8eed55be2109b6d680b41795e433618d46eaa
Florian Sanders <sanders.florian@gmail.com>
2021-11-22 00:18:44 +0100

    feat: updated config to generate static site

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 
 import { resolve } from 'path';
 
+import adapter from "@sveltejs/adapter-static";
+
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,6 +14,12 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: "build",
+			assets: "build",
+			fallback: null,
+		  }),
 		vite: {
 			resolve: {
 				alias: {
