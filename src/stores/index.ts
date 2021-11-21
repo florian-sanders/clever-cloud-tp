357a433b62f3183771b0dad3bfcc5d58501c8600
Florian Sanders <sanders.florian@gmail.com>
2021-11-21 21:15:03 +0100

    feat: store - v1

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,16 @@
-import { writable } from "svelte/store";
+import { writable, derived } from 'svelte/store';
+import type { CartVariant, Variant, CartItem, SelectedVariant } from '$interfaces';
 
-export const count = writable(0);
+export const variants = writable<Array<Variant>>([]);
+
+export const cartVariants = writable<Array<CartVariant>>([]);
+
+export const cartItems = writable<Array<CartItem>>([]);
+
+export const currentVariantId = writable('');
+
+export const selectedVariant = writable<SelectedVariant | undefined>();
+
+export const total = derived(cartItems, ($cartItems) =>
+	$cartItems.reduce((sum, cartItem) => sum + cartItem.flavor.price, 0)
+);
