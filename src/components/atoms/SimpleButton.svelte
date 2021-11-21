a8bf0da649f47157d533922ee30c8fe0bbc17af5
Florian Sanders <sanders.florian@gmail.com>
2021-11-21 21:03:03 +0100

    feat: SimpleButton component - v1

--- src/components/atoms/SimpleButton.svelte
+++ src/components/atoms/SimpleButton.svelte
@@ -6,70 +6,66 @@
 
 	@prop {function} action - Callback to be triggered on click
 	@prop {string} text - Text displayed inside the button
-	@prop {'primary' | 'secondary' | 'cautionary'} skin - Visual appearance of the button
-		@default 'primary'
+	@prop {'primary' | 'secondary' | 'cautionary'} skin - Visual appearance of the button (default='primary') 		
+	@prop {isPushButton} boolean - If true, button is a toggle button alternating between states (default=false)
+	@prop {isPressed} boolean - State of the toggle button (pressed or not pressed) (default=false)
  -->
-
 <script lang="ts">
 	export let action: () => void;
 	export let text: string;
 	export let skin: 'primary' | 'secondary' | 'cautionary' = 'primary';
+	export let isDisabled: boolean = false;
 </script>
 
-<button class={skin} on:click={action} type="button">{text}</button>
+<button
+	class={skin}
+	on:click|stopPropagation={action}
+	type="button"
+	disabled={isDisabled}
+	style={`--component-color: var(--${skin}-color)`}
+>
+	{text}
+	<slot name="img" />
+</button>
 
 <style>
 	/* basic styles */
 	button {
-		border: solid 2px transparent;
-		padding: 0.5rem 1rem;
+		height: fit-content;
+		padding: 0.5rem 0.8rem;
 		border-radius: 0.3rem;
+		border: solid 1px transparent;
+		box-shadow: var(--shadow-elevation-high);
 		color: var(--white-color);
-		box-shadow: var(--shadow-closer);
+		font-weight: bold;
+		cursor: pointer;
 		transition: background-color 0.4s ease-out;
 	}
 
-	button.primary {
-		background: var(--primary-color);
-	}
-
-	button.secondary {
-		background: var(--secondary-color);
-	}
-
+	button.primary,
+	button.secondary,
 	button.cautionary {
-		background: var(--cautionary-color);
+		background: var(--component-color);
 	}
 
 	/* hover + focus styles */
 	button:hover,
 	button:focus-visible {
-		background-color: transparent;
-		border: solid 2px var(--primary-color);
+		border: solid 1px var(--component-color);
+		background-color: var(--white-color);
 		color: var(--black-color);
 		transition: background-color 0.4s ease-out, color 0.4s ease-out, border 1s ease-out;
 	}
 
 	button:focus-visible {
-		outline: dashed 2px var(--primary-color);
+		outline: dashed 2px var(--component-color);
 		outline-offset: 5px;
 	}
 
-	button.primary:hover,
-	button.primary:focus-visible {
-		background: transparent;
-		border: solid 2px var(--primary-color);
-	}
-
-	button.secondary:hover,
-	button.secondary:focus-visible {
-		background: transparent;
-		border: solid 2px var(--secondary-color);
-	}
-
-	button.cautionary:hover,
-	button.cautionary:focus-visible {
-		background: transparent;
-		border: solid 2px var(--cautionary-color);
+	button[disabled] {
+		box-shadow: none;
+		background-color: var(--soft-black-color);
+		color: var(--white-color);
+		cursor: auto;
 	}
 </style>
