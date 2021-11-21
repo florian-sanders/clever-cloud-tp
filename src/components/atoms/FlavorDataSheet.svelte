d5fd35715adf16b5cdf63bba5058d9b2af7075c2
Florian Sanders <sanders.florian@gmail.com>
2021-11-21 21:12:14 +0100

    feat: Added component for flavor data sheet

---
+++ src/components/atoms/FlavorDataSheet.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import type { Flavor } from '$interfaces';
+
+    import { getFormattedRam } from '$utilities';
+
+    export let flavor: Flavor;
+</script>
+
+<dl>
+    <dt>CPUs:</dt>
+    <dd>{flavor.cpus}</dd>
+    <dt>GPUs:</dt>
+    <dd>{flavor.gpus}</dd>
+    <dt>RAM:</dt>
+    <dd>{getFormattedRam(flavor.mem)}</dd>
+</dl>
+<dl>
+    <dt>Microservice:</dt>
+    <dd>
+        {#if flavor.microservice}
+            <img src="/img/check-circle.svg" alt="yes" width="16" />
+        {:else}
+            <img src="/img/cross-circle.svg" alt="no" width="16" />
+        {/if}
+    </dd>
+    <dt>Machine learning:</dt>
+    <dd>
+        {#if flavor.machine_learning}
+            <img src="/img/check-circle.svg" alt="yes" width="16" />
+        {:else}
+            <img src="/img/cross-circle.svg" alt="no" width="16" />
+        {/if}
+    </dd>
+</dl>
+
+<style>
+    dl {
+		flex-grow: 5;
+		display: grid;
+		grid-template-columns: auto 1fr;
+		row-gap: calc(var(--gutter) / 2);
+		align-items: center;
+		width: max-content;
+		font-size: 0.8rem;
+	}
+
+	dd {
+		margin: 0;
+	}
+
+	dd img {
+		display: block;
+		margin: 0;
+		width: 1rem;
+		height: 1rem;
+	}
+
+	dt {
+		margin-right: calc(var(--gutter) / 2);
+		color: var(--primary-color);
+		text-transform: capitalize;
+	}
+</style>
