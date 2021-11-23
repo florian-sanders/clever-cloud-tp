afc1da869010650df60e455a0d0419c08372b4a9
Florian Sanders <sanders.florian@gmail.com>
2021-11-23 22:58:29 +0100

    doc+clean: documented stored + cleaned TS interfaces and removed unused stores

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,6 @@
 import type { Flavor } from "./Flavor";
 import type { Variant } from "./Variant";
-import type { CartVariant } from "./CartVariant";
 import type { CartItem } from "./CartItem";
 import type { SelectedVariant } from "./SelectedVariant";
 
-export type { Flavor, Variant, CartVariant, CartItem, SelectedVariant };
+export type { Flavor, Variant, CartItem, SelectedVariant };
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,18 +1,19 @@
+/* Svelte imports */
 import { writable, derived } from 'svelte/store';
-import type { CartVariant, Variant, CartItem, SelectedVariant } from '$interfaces';
 
+/* TypeScript imports */
+import type { Variant, CartItem, SelectedVariant } from '$interfaces';
+
+/* Stores */
 export const loading = writable(true);
 
 export const variants = writable<Array<Variant>>([]);
 
-export const cartVariants = writable<Array<CartVariant>>([]);
-
 export const cartItems = writable<Array<CartItem>>([]);
-
-export const currentVariantId = writable('');
 
 export const selectedVariant = writable<SelectedVariant | undefined>();
 
+/* Updates and exports a "total" store everytime cartItems store is modified */
 export const total = derived(cartItems, ($cartItems) =>
 	$cartItems.reduce((sum, cartItem) => sum + cartItem.flavor.price, 0)
 );
