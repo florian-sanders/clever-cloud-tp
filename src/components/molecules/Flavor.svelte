160b66b8bc2e952713bf2740702e75aea1fd3077
Florian Sanders <sanders.florian@gmail.com>
2021-11-23 23:52:25 +0100

    doc: documented Flavor component

--- src/components/molecules/Flavor.svelte
+++ src/components/molecules/Flavor.svelte
@@ -4,21 +4,36 @@
 
 	Flavor card
 
-	@prop {Flavor} flavor 
+	@prop {Flavor} flavor (see interface below)
+	interface Flavor {
+		name: string;
+		cpus: number;
+		gpus: number;
+		mem: number;
+		price: number;
+		price_id: string;
+		microservice: boolean;
+		machine_learning: boolean;
+	}
  -->
 <script lang="ts">
-	import type { Flavor, CartItem } from '$interfaces';
+	/* Component imports */
 	import FlavorDataSheet from '$components/atoms/FlavorDataSheet.svelte';
 	import SimpleButton from '$components/atoms/SimpleButton.svelte';
 	import FluidCard from '$components/molecules/FluidCard.svelte';
+
+	/* TypeScript imports */
+	import type { Flavor } from '$interfaces';
+
+	/* stores and helper imports */
 	import { selectedVariant, cartItems } from '$stores';
 	import { getFormattedPrice } from '$utilities';
 
 	export let flavor: Flavor;
 
 	let addFlavorToCart = () =>
-		cartItems.update((previousCartItems) => [
-			...previousCartItems,
+		($cartItems = [
+			...$cartItems,
 			{
 				variantInfo: { ...$selectedVariant },
 				flavor: { ...flavor }
@@ -31,8 +46,6 @@
 	<FlavorDataSheet {flavor} />
 	<svelte:fragment slot="price">
 		<p class="price">€&nbsp;{getFormattedPrice(flavor.price)}</p>
-		<SimpleButton action={addFlavorToCart} skin="secondary">
-			Add to estimate
-		</SimpleButton>
+		<SimpleButton action={addFlavorToCart} skin="secondary">Add to estimate</SimpleButton>
 	</svelte:fragment>
 </FluidCard>
